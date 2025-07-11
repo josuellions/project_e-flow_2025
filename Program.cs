c2476ce49d6d35379c456392713003594d9bcef7
josuel-developerHF <josuel.lopes@hfbr.com.br>
2025-07-11 19:53:28 -0300

    feat: adds structure, responsibilities, classes and service and encapsulate the main logic

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TesteTecnico.src.Controllers;
+using TesteTecnico.src.UseCases.GerarXMLNota.PostCurrent;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                // Controller
+                services.AddTransient<NotaController>();
+
+                // UseCase ou Service injetado na controller
+                services.AddTransient<PostCurrentGerarXMLNota>();
+
+                // Se PostCurrentGerarXMLNota tiver outras dependências, registre aqui também
+            })
+            .Build();
+
+        //dotnet run -- gerar  -- modelo 55 -- tipo saida .\TesteTecnico.csproj 
+        //dotnet run -- listar  -- modelo 55 -- tipo saida .\TesteTecnico.csproj 
+
+        var isDev = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+
+        #if DEBUG
+        // Verifica se está no ambiente de desenvolvimento e nenhum argumento foi passado
+
+        if (isDev && args.Length == 0)
+        {
+            // Força argumento "gerar" ou "listar" para teste automático
+            args = new[] { "gerar", "modelo", "55", "tipo", "saida" };
+            //args = new[] { "listar", "modelo", "55", "tipo", "saida" };
+        }
+        #endif
+
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Informe um comando: gerar ou listar, modelo e tipo");
+            
+            #if DEBUG
+            Console.ReadLine();
+            #endif
+
+            return;
+        }
+
+        var argumentos = args.Skip(1).ToArray();
+        var comando = args[0].ToLower();
+        var comandos = new Dictionary<string, Func<string[], Task>>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["gerar"] = NFeApp.GerarXMLAsync,
+            ["listar"] = NFeApp.ListarXMLAsync
+        };
+
+        if (comandos.TryGetValue(args[0], out var executar))
+        {
+            await executar(argumentos);
+        }
+        else
+        {
+            Console.WriteLine($">>{args[0]}: Comando inválido! Use 'gerar' ou 'listar'.");
+        }
+
+        #if DEBUG
+        Console.WriteLine("\n[DEBUG] Pressione Enter para sair...");
+        Console.ReadLine();
+        #endif
+    }
+}
---
+++ src/UseCases/GerarXMLNota/GetCurrent/GetCurrentListaXMLNota.cs
@@ -0,0 +1,31 @@
+﻿using TesteTecnico.Communication.Responses;
+using TesteTecnico.src.Contracts;
+
+namespace TesteTecnico.src.UseCases.GerarXMLNota.GetCurrent
+{
+    public class GetCurrentListaXMLNota 
+    {
+        private readonly INotaRepository _repository;
+
+        public GetCurrentListaXMLNota(INotaRepository repository) => _repository = repository;
+
+        /// <summary>
+        /// Implementa a regra de negocio retornar os arquivos XML de notas fiscais gerados e salvos
+        /// </summary>
+        /// <param name="tipo"></param>
+        /// <param name="modelo"></param>
+        /// <returns></returns>
+
+        // Retorna os arquivos dos últimos 7 dias com base na data embutida na chave
+        public async Task<List<ResponseNotaFiscal>> ObterNotasUltimos7Dias(string tipo, string modelo)
+        {
+            var dataFim = DateTime.Today;
+            var dataInicio= dataFim.AddDays(-7);
+
+            var result = await Task.Run(() =>  _repository.ListaPorPeriodo(dataInicio, dataFim, modelo));
+
+            return result;
+    
+        }
+    }
+}
